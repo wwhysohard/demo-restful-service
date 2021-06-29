0e697515d8a96d194d0b80a35ee5b3664f4bcdf0
whysohard <47knursafi@gmail.com>
2021-06-29 13:07:57 +0600

    some changes

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -11,13 +11,12 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
@@ -27,25 +26,31 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping
+    @PostMapping("/registration")
+    public User register(@RequestBody User user) {
+        userService.saveUser(user);
+        return user;
+    }
+
+    @GetMapping("/users")
     @PreAuthorize("hasAuthority('user:read')")
     public List<User> findAll() {
         return userService.findAll();
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/users/{id}")
     @PreAuthorize("hasAuthority('user:read')")
     public User findById(@PathVariable("id") Integer id) {
         return userService.findById(id);
     }
 
-    @PutMapping("/update/{id}")
+    @PutMapping("/users/update/{id}")
     @PreAuthorize("hasAuthority('user:modify')")
     public User updateUser(@PathVariable("id") Integer id, @RequestBody RoleDTO newUserRole) {
         return userService.updateUserRole(id, newUserRole);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/users/{id}")
     @PreAuthorize("hasAuthority('user:modify')")
     public void deleteUser(@PathVariable("id") Integer id) {
         userService.deleteById(id);
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -12,16 +12,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service("UserService")
 public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
     }
 
     public List<User> findAll() {
